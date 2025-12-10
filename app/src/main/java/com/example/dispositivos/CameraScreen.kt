4a558be58b4994aef67d9f59958c2d38f4bd9c4d
Ronald Bismar <ronald.limachi@hansa.com.bo>
2025-12-10 00:18:25 -0400

    Funcionalidad de Camara 6

--- app/src/main/java/com/example/dispositivos/CameraScreen.kt
+++ app/src/main/java/com/example/dispositivos/CameraScreen.kt
@@ -158,7 +158,7 @@ class CameraScreenActivity : ComponentActivity() {
                 put(MediaStore.MediaColumns.DISPLAY_NAME, name)
                 put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
                 if (Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
-                    put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/CameraX-Image")
+                    put(MediaStore.Images.Media.RELATIVE_PATH, "DCIM/Camera")
                 }
             }
 
